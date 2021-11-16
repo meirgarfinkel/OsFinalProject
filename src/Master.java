04a8b33bc360eef0d20eab652f0f9d88d02e7223
mordye137 <mordymepstein@gmail.com>
2021-11-16 17:01:42 -0500

    Adjusted comments and removed unnecessary code

--- src/Master.java
+++ src/Master.java
@@ -18,39 +18,21 @@ public class Master {
         int portNumber = Integer.parseInt(args[0]);
 
         try (ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
-             //
+             //Create slave sockets
              Socket SlaveA = serverSocket.accept();
              Socket SlaveB = serverSocket.accept();
 
-             PrintWriter SLaveAOutWriter = new PrintWriter(SlaveA.getOutputStream(), true);
-             PrintWriter SLaveBOutWriter = new PrintWriter(SlaveB.getOutputStream(), true);
+             //Create slave writers
+             PrintWriter SlaveAOutWriter = new PrintWriter(SlaveA.getOutputStream(), true);
+             PrintWriter SlaveBOutWriter = new PrintWriter(SlaveB.getOutputStream(), true);
 
+             //Create slave readers
              BufferedReader SlaveAInReader = new BufferedReader(new InputStreamReader(SlaveA.getInputStream()));
              BufferedReader SlaveBInReader = new BufferedReader(new InputStreamReader(SlaveB.getInputStream()));
         ) {
-
-            //Packet Creator creates packets with assigned indexes
-            packetList = packetCreator.createPackets();
-            //Create the last packet with special index and total size as data
-            Packet lastPacket = new Packet(-1, String.valueOf(packetList.size()));
-
             while (true) {
 
                 System.out.println("Connected to client.");
-
-                //Creates list of shuffled packets with a 80% chance of being "dropped"
-                ArrayList<Packet> toSend = eightyPercentList(packetList);
-                //Add the last packet to the end of the list
-                toSend.add(lastPacket);
-
-                System.out.println("Sending packets");
-
-                for (Packet p : toSend ) {
-                    outWriter.println(p.toStringIndex());
-                    outWriter.println(p.toStringData());
-                }
-
-                System.out.println("Packets Sent");
 
                 //Listens for a message from the client to see if it is done sending packets
                 if ((inReader.readLine()).equals("FINISHED")) {
@@ -65,17 +47,5 @@ public class Master {
                     "Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
             System.out.println(e.getMessage());
         }
-    }
-
-    //Method to "drop" 80% of packets
-    private static ArrayList<Packet> eightyPercentList(ArrayList<Packet> packets){
-        Random rand = new Random();
-        ArrayList<Packet> batch = new ArrayList<>();
-
-        for (Packet packet : packets) {
-            if (rand.nextInt(10)<8)
-                batch.add(packet);
-        }
-        return batch;
     }
 }
